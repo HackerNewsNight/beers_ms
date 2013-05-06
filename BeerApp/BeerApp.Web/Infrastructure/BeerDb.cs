d38629a9cb0bc41a02d40413b667b38c0dd69f89
Darren Manners <darrenmanners@gmail.com>
2013-05-07 07:38:05 +1000

    Basic Input: Initial Solution Design
    First looks at the Beer App using Visual Studio's MVC Website Template.

---
+++ BeerApp/BeerApp.Web/Infrastructure/BeerDb.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using BeerApp.Domain;
+
+namespace BeerApp.Web.Infrastructure
+{
+    public class BeerDb : DbContext, IBeerDataSource
+    {
+        public DbSet<Beer> Beers { get; set; }
+        public DbSet<Entry> Entries { get; set; }
+
+        IQueryable<Beer> IBeerDataSource.Beers
+        {
+            get { return Beers; }
+        }
+
+        IQueryable<Entry> IBeerDataSource.Entries
+        {
+            get { return Entries; }
+        }
+    }
+}
---
+++ BeerApp/BeerApp.Web/Migrations/Configuration.cs
@@ -0,0 +1,34 @@
+﻿namespace BeerApp.Web.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<BeerApp.Web.Infrastructure.BeerDb>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(BeerApp.Web.Infrastructure.BeerDb context)
+        {
+            /* todo: dmanners - call in the data from the csv files. */
+            //  This method will be called after migrating to the latest version.
+
+
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
