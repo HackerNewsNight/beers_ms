f62ae076220ba61f460d3a5dca6595e33c438207
Darren Manners <darrenmanners@gmail.com>
2013-06-15 21:37:09 +1000

    Increasing separation for data.
    The entity framework is only referenced in its own project. If we want
    to separate it to a different framework later one.. it wont be tightly
    coupled.

---
+++ BeerApp.API/BeerApp.Data/DataContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BeerApp.Models;
+
+namespace BeerApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base(DataContext.ConnectionStringName) { }
+
+        public DbSet<Beer> Beers { get; set;  }
+
+        public static string ConnectionStringName 
+        { 
+            get
+            {
+                if(ConfigurationManager.AppSettings["ConnectionStringName"] != null)
+                {
+                    return ConfigurationManager.AppSettings["ConnectionStringName"]; 
+                }
+                return "DefaultConnection";
+            }
+        }
+    }
+}
