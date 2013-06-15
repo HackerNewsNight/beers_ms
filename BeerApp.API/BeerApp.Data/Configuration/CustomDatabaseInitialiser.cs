5938ad3b631345727dbcfc1e02955033c59afe08
Darren Manners <darrenmanners@gmail.com>
2013-06-15 22:19:00 +1000

    Project References + XML File
    Added xml file as a reference to the Data project..
    Also added a custom database initialiser that will be the new seed.
    Going to install sql2012 to later connect to this. hopefully the local
    mdf file will work in this new context.

---
+++ BeerApp.API/BeerApp.Data/Configuration/CustomDatabaseInitialiser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure; 
+
+namespace BeerApp.Data.Configuration
+{
+    public class CustomDatabaseInitialiser : CreateDatabaseIfNotExists<DataContext>
+    {
+        protected override void Seed(DataContext context)
+        {
+            
+            
+            base.Seed(context);
+        }
+
+        public CustomDatabaseInitialiser()
+        {
+
+        }
+    }
+}
