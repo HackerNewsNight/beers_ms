4140c14150411826e0756dc510a84c2e35d98142
Darren Manners <darrenmanners@gmail.com>
2013-06-15 16:35:37 +1000

    Restructure solutions..

---
+++ BeerApp.API/BeerApp.API/Migrations/Configuration.cs
@@ -0,0 +1,167 @@
+namespace BeerApp.Web.API.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<BeerApp.Web.Infrastructure.BeerDb>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(BeerApp.Web.Infrastructure.BeerDb context)
+        {
+
+        }
+            /* todo: dmanners - call in the data from the csv files. */
+            //  This method will be called after migrating to the latest version.
+
+            //var xmlDoc = XDocument.Load(@"d:\temp\beers_all.xml");
+            //var q = from data in xmlDoc.Descendants("table_data")
+            //        let attribute = data.Attribute("name")
+            //        where attribute != null && attribute.Value == "beers"
+            //        select data; 
+            
+            //IEnumerable<XElement> beers =
+            //    from c in xmlDoc.Elements("table_data")
+            //    where (string)c.Attribute("name") == "beers"
+            //    select c;
+            
+            //var categories = PopulateCategories(context);
+            //var styles = PopulateStyles(context, categories);
+            //var brewers = PopulateBrewers(context);
+            //var beers = OpenDbBeers(context, styles, brewers);
+            
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+
+        //private static string[] OpenDbBeers(BeerDb context, IEnumerable<Style> styles, IEnumerable<Brewer> brewers)
+        //{
+        //    var openDbBeers = File.ReadAllLines(@"d:\temp\beers.csv");
+        //    // "id","brewery_id","name","cat_id","style_id","abv","ibu","srm","upc","filepath","descript","last_mod"
+        //    var beers = (from line in openDbBeers
+        //                 let data = line.Split(',')
+        //                 select new
+        //                            {
+        //                                id = Int32.Parse(data[0]),
+        //                                brewery_id = data[1],
+        //                                name = data[2],
+        //                                cat_id = data[3],
+        //                                style_id = data[4],
+        //                                abv = data[5],
+        //                                ibu = data[6],
+        //                                srm = data[7],
+        //                                upc = data[8],
+        //                                filepath = data[9],
+        //                                descript = data[10],
+        //                                last_mod = data[11]
+        //                            }).Select(x => new Beer()
+        //                                               {
+        //                                                   Id = x.id,
+        //                                                   Name = x.name,
+        //                                                   Style = styles.FirstOrDefault(style => style.Id == x.id),
+        //                                                   Brewer = brewers.FirstOrDefault(brewer => brewer.Id == x.id)
+        //                                               }).ToArray();
+        //    context.Beers.AddOrUpdate(x => x.Id, beers);
+        //    context.SaveChanges();
+        //    return openDbBeers;
+        //}
+
+        //private static IEnumerable<Brewer> PopulateBrewers(BeerDb context)
+        //{
+        //    var openDbBrewer = File.ReadAllLines(@"d:\temp\breweries.csv");
+        //    // id	name	address1	address2	city	state	code	country	phone	website	filepath	descript	last_mod
+        //    var breweries = (from line in openDbBrewer
+        //                     let data = line.Split(',')
+        //                     where data.Count()> 8 && !string.IsNullOrEmpty(data[0])
+        //                     select new
+        //                                {
+        //                                    id = Int32.Parse(data[0]),
+        //                                    name = data[1],
+        //                                    address1 = data[2],
+        //                                    address2 = data[3],
+        //                                    city = data[4],
+        //                                    state = data[5],
+        //                                    code = data[6],
+        //                                    country = data[7],
+        //                                    phone = data[8],
+        //                                    website = data[9],
+        //                                    filepath = data[10],
+        //                                    descript = data[11]
+        //                                }).Select(x => new Brewer()
+        //                                                   {
+        //                                                       Id = x.id,
+        //                                                       Name = x.name,
+        //                                                       Contact = new ContactDetails()
+        //                                                                     {
+        //                                                                         Address1 = x.address1,
+        //                                                                         Address2 = x.address2,
+        //                                                                         City = x.city,
+        //                                                                         Country = x.country,
+        //                                                                         Phone = x.phone,
+        //                                                                         State = x.state,
+        //                                                                         Website = x.website
+        //                                                                     }
+        //                                                   }).ToArray();
+        //    context.Brewers.AddOrUpdate(x => x.Id, breweries);
+        //    context.SaveChanges(); 
+        //    return breweries; 
+        //}
+
+        //private static IEnumerable<Style> PopulateStyles(BeerDb context, IEnumerable<Category> categories)
+        //{
+        //    var openDbStyles = File.ReadAllLines(@"d:\temp\styles.csv");
+        //    // id	cat_id	style_name	last_mod
+        //    var styles = (from line in openDbStyles
+        //                  let data = line.Split(',')
+        //                  select new
+        //                             {
+        //                                 id = Int32.Parse(data[0]),
+        //                                 cat_id = Int32.Parse(data[1]),
+        //                                 style_name = data[2],
+        //                                 last_mod = data[3]
+        //                             }).Select(
+        //                                 x =>
+        //                                 new Style()
+        //                                     {
+        //                                         Id = x.id,
+        //                                         Name = x.style_name,
+        //                                         Category = categories.FirstOrDefault(cat => cat.Id == x.cat_id)
+        //                                     }).ToArray();
+        //    context.Styles.AddOrUpdate(x => x.Id, styles);
+        //    context.SaveChanges();
+        //    return styles;
+        //}
+
+        //private static IEnumerable<Category> PopulateCategories(BeerDb context)
+        //{
+        //    var openDbCategories = File.ReadAllLines(@"d:\temp\categories.csv");
+        //    // id	cat_name	last_mod
+        //    var categories = (from line in openDbCategories
+        //                      let data = line.Split(',')
+        //                      select new
+        //                                 {
+        //                                     id = Int32.Parse(data[0]),
+        //                                     cat_name = data[1],
+        //                                     last_mod = DateTime.Parse(data[2])
+        //                                 }).Select(x => new Category()
+        //                                                    { 
+        //                                                        Id = x.id,
+        //                                                        Name = x.cat_name,
+        //                                                        Modified = x.last_mod
+        //                                                    }).ToArray();
+        //    context.Categories.AddOrUpdate(x => x.Id, categories);
+        //    context.SaveChanges(); 
+        //    return categories;
+        //}
+ 
+}
---
+++ BeerApp.Web/BeerApp.Web.Tests/DB.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BeerApp.Web.Tests
+{
+    [TestClass]
+    public class DB
+    {
+        [TestMethod]
+        public void DB_Load()
+        {
+            var xmlDoc = XDocument.Load(@"d:\temp\beers_all.xml");
+            var beers = xmlDoc.Descendants("table_data")
+                .Where(x =>
+                           {
+                               var attribute = x.Attribute("name");
+                               return attribute != null && attribute.Value == "beers";
+                           }).Descendants("row").ToList();
+
+            foreach (var xElement in beers)
+            {
+                Console.Write(xElement);
+            }
+        }
+    }
+}
