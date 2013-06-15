30845d4db23743a8154b7cfd8738659fa93da074
Stefano Charissis <stefano.charissis@gmail.com>
2013-06-15 17:38:41 +1000

    New: Added bootstrap.

---
+++ BeerApp.Web/BeerApp.Web/Scripts/angularjs/Controllers/HomeController.js
@@ -0,0 +1,9 @@
+﻿function HomeController($scope, $http) {
+    // Test beer data
+    // TODO: GET /api/beers
+    $scope.beers = [
+        { name: "Beer 1" },
+        { name: "Beer 2" },
+        { name: "Beer 3" }
+    ];
+}
